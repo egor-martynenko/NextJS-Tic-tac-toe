98d076b7dff99a5e8cf93d4de9583bb2f7079646
egormartynenko <egor.martinenko01@gmail.com>
2025-11-22 13:52:45 +0300

    add base structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <Button size="lg" variant="default">Hello</Button>
+    </div>
+  );
+}
